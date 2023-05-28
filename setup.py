40aa92420e72c2980d635be3d59d0948754b698a
dusklight00 <r.rahul.developer@gmail.com>
2023-05-28 16:13:58 +0530

    fixed the readme issue coming on pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    readme = f.read()
+
 setup(
     name="webscapy",
-    version="1.2.0",
+    version="1.2.1",
     description="Selenium built for scraping instead of testing",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     author="Rahul Raj",
     packages=["webscapy"],
     zip_safe=False
