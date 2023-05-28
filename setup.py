0289d60a9be18e35a66e741d5256686a1e92d79c
dusklight00 <r.rahul.developer@gmail.com>
2023-05-28 14:39:08 +0530

    documented introduction, features and installation guides

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="webscapy",
-    version="1.0.0",
+    version="1.1.0",
     description="Selenium built for scraping instead of testing",
     author="Rahul Raj",
     packages=["webscapy"],
